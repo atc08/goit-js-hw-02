b0a9746735bc41ebf933e4286838906d73dc19f5
Yevhenii Korkots <atc0884@gmail.com>
2020-07-16 00:41:23 +0300

    change title

--- js/task-02.js
+++ js/task-02.js
@@ -1,27 +1,27 @@
-"use strict";
+// "use strict";
 
-const calculateEngravingPrice = function (message, pricePerWord) {
-  return message.split(" ").length * pricePerWord;
-};
+// const calculateEngravingPrice = function (message, pricePerWord) {
+//   return message.split(" ").length * pricePerWord;
+// };
 
-console.log(
-  calculateEngravingPrice(
-    "Proin sociis natoque et magnis parturient montes mus",
-    10
-  )
-);
+// console.log(
+//   calculateEngravingPrice(
+//     "Proin sociis natoque et magnis parturient montes mus",
+//     10
+//   )
+// );
 
-console.log(
-  calculateEngravingPrice(
-    "Proin sociis natoque et magnis parturient montes mus",
-    20
-  )
-);
+// console.log(
+//   calculateEngravingPrice(
+//     "Proin sociis natoque et magnis parturient montes mus",
+//     20
+//   )
+// );
 
-console.log(
-  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)
-);
+// console.log(
+//   calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)
+// );
 
-console.log(
-  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)
-);
+// console.log(
+//   calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)
+// );
--- js/task-05.js
+++ js/task-05.js
@@ -1,26 +1,26 @@
-"use strict";
+// "use strict";
 
-const checkForSpam = function (message) {
-  let isTrash =
-    message.toLowerCase().includes("spam") ||
-    message.toLowerCase().includes("sale");
-  if (isTrash) {
-    return "true";
-  } else {
-    return "false";
-  }
+// const checkForSpam = function (message) {
+//   let isTrash =
+//     message.toLowerCase().includes("spam") ||
+//     message.toLowerCase().includes("sale");
+//   if (isTrash) {
+//     return "true";
+//   } else {
+//     return "false";
+//   }
 
-  /* Через тернарный оператор*/
-  // return message.toLowerCase().includes("spam") ||
-  //   message.toLowerCase().includes("sale")
-  //   ? true
-  //   : false;
-};
+//   /* Через тернарный оператор*/
+//   // return message.toLowerCase().includes("spam") ||
+//   //   message.toLowerCase().includes("sale")
+//   //   ? true
+//   //   : false;
+// };
 
-console.log(checkForSpam("Latest technology news")); // false
+// console.log(checkForSpam("Latest technology news")); // false
 
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
+// console.log(checkForSpam("JavaScript weekly newsletter")); // false
 
-console.log(checkForSpam("Get best sale offers now!")); // true
+// console.log(checkForSpam("Get best sale offers now!")); // true
 
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
