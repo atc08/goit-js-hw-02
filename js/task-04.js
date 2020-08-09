7786e97f1e665a792fadf42bdceec7a2f91c78d2
Yevhenii Korkots <atc0884@gmail.com>
2020-08-10 00:29:42 +0300

    seventh task

--- js/task-04.js
+++ js/task-04.js
@@ -1,24 +1,24 @@
-// "use strict";
+"use strict";
 
-// const formatString = function (string) {
-//   if (string.length <= 40) {
-//     return string;
-//   } else {
-//     return string.slice(0, 40) + "...";
-//   }
+const formatString = function (string) {
+  if (string.length <= 40) {
+    return string;
+  } else {
+    return string.slice(0, 40) + "...";
+  }
 
-//   /* Через тернарик*/
-//   // return string.length < 40 ? string : string.slice(0, 40) + "...";
-// };
+  /* Через тернарик*/
+  // return string.length < 40 ? string : string.slice(0, 40) + "...";
+};
 
-// console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+console.log(formatString("Curabitur ligula sapien, tincidunt non."));
 
-// console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
 
-// console.log(formatString("Curabitur ligula sapien."));
+console.log(formatString("Curabitur ligula sapien."));
 
-// console.log(
-//   formatString(
-//     "Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."
-//   )
-// );
+console.log(
+  formatString(
+    "Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."
+  )
+);
